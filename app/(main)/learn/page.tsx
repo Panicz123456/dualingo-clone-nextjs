354b3c4d3bb4f44fff9c5f7eba785df721ec193b
patryk <paniczgg@gmail.com>
2024-12-25 08:21:47 +0100

    dodanie pare funkci itd

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,5 +1,34 @@
-const LearnPage = () => {
-  return <div>LearnPage</div>;
+import { FeedWrapper } from "@/components/feed-wrapper";
+import { Header } from "@/app/(main)/learn/header";
+import { StickyWrapper } from "@/components/StickyWrapper";
+import { UserProgress } from "@/components/user-progress";
+import { getUserProgress } from "@/db/queries";
+import { redirect } from "next/navigation";
+
+const LearnPage = async () => {
+  const userProgressData = getUserProgress();
+
+  const [userProgress] = await Promise.all([userProgressData]);
+
+  if (!userProgress || !userProgress.activeCourse) {
+    redirect("/courses");
+  }
+
+  return (
+    <div className="flex flex-row-reverse gap-[48px] px-6">
+      <StickyWrapper>
+        <UserProgress
+          activeCourse={userProgress.activeCourse}
+          hearts={userProgress.hearts}
+          points={userProgress.points}
+          hasActiveSubscription={false}
+        />
+      </StickyWrapper>
+      <FeedWrapper>
+        <Header title={userProgress.activeCourse.title} />
+      </FeedWrapper>
+    </div>
+  );
 };
 
 export default LearnPage;
